88dea872dc6cd6cd14a3fb03393e3e165604a2ce
JonesNithyanandam <86592331+JonesNithyanandam@users.noreply.github.com>
2021-07-04 12:33:58 +0530

    Add files via upload

---
+++ 1C-IMP/src/app/Login/loginmaster/loginmaster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginmaster',
+  templateUrl: './loginmaster.component.html',
+  styleUrls: ['./loginmaster.component.scss']
+})
+export class LoginmasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
